54acf9e463ba81b22c273416c7d283d900384a89
Vy Ho <vyho0711@gmail.com>
2018-10-16 23:10:40 -0400

    Create docstrings.py

---
+++ docstrings.py
@@ -0,0 +1,19 @@
+def myFunction(arg1,arg2 = None) #function parameters
+
+"""myFunction(arg1,arg2 = None) --> this functions does x
+
+Parameters:
+    arg1: the first argument is ...
+    arg2: the 2nd argument is ...
+    
+"""
+    print(arg1,arg2)
+    
+    
+def main():
+    print(myFunction.__doc__)
+    
+    
+if __name__ == "__main__":
+    
+    main()
