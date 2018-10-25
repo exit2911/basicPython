85eb65fd5ad808cc948ae6f0ec50e1ca0f6688f5
Vy Ho <vyho0711@gmail.com>
2018-10-24 20:47:15 -0400

    Create basicLogging.py

---
+++ basicLogging.py
@@ -0,0 +1,31 @@
+import logging
+
+"""
+
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+# INFO: Confirmation that things are working as expected.
+
+# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
+"""
+
+
+
+
+
+def main():
+    
+    logging.basicConfig(level = logging.DEBUG,filemode='w',filename='output.log',format = '%(asctime)s:%(levelname)s:%(message)s')
+    
+    logging.debug("message")
+    
+    logging.info("here's a {} variable and an integer {}".format("string",10))
+    
+if __name__ == "__main__":
+
+    main()
