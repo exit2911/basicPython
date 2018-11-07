38588963d1d8fef972f2375c7bb237f3cf5b280e
Vy Ho <vyho0711@gmail.com>
2018-11-06 23:18:12 -0500

    Create functionwArbiArg.py

---
+++ functionwArbiArg.py
@@ -0,0 +1,9 @@
+def make_pizza(*toppings):
+    
+    """print the list of toppings that have been requested"""
+    print(toppings)
+    
+    
+make_pizza('pepperoni')
+
+make_pizza('J','K','Q','A')
